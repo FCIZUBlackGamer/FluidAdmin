fb9e7631febfaf258f6689b8cfa428dc0a408659
Nesma20 <nesmatharwat20@gmail.com>
2020-06-27 12:43:18 +0200

    push changes

--- app/src/main/java/com/thetatechno/fluidadmin/utils/Validation.java
+++ app/src/main/java/com/thetatechno/fluidadmin/utils/Validation.java
@@ -12,7 +12,6 @@ public class Validation {
     private static String TAG = "Validation";
     private static final int PASSWORD_LENGTH = 6;
 
-
     public static boolean isValidEmail(String email) {
         return (TextUtils.isEmpty(email.trim()) || Patterns.EMAIL_ADDRESS.matcher(email).matches());
 
@@ -21,26 +20,32 @@ public class Validation {
     public static boolean isValidPassword(String password) {
         return password.length() >= PASSWORD_LENGTH;
     }
-    public static boolean isValidPhoneNumber(String phone){
-        Log.i(TAG,Patterns.PHONE.pattern());
+
+    public static boolean isValidPhoneNumber(String phone) {
+        Log.i(TAG, Patterns.PHONE.pattern());
         return (TextUtils.isEmpty(phone.trim()) || phone.matches("^[+]?[0-9]{10,15}$"));
     }
-    public static boolean isValidForName(String word){
 
-        return (!TextUtils.isEmpty(word.trim())&& word.matches("[a-zA-Z]+"));
+    public static boolean isValidForName(String word) {
+
+        return (!TextUtils.isEmpty(word.trim()) && word.matches("[a-zA-Z]+"));
     }
-    public static boolean isValidForId(String id){
+
+    public static boolean isValidForId(String id) {
 
         return (!TextUtils.isEmpty(id.trim()));
     }
-    public static boolean isValidForSpeciality(String speciality){
+
+    public static boolean isValidForSpeciality(String speciality) {
 
         return (!TextUtils.isEmpty(speciality.trim()));
     }
-    public static boolean isValidForWord(String word){
+
+    public static boolean isValidForWord(String word) {
 
         return (!TextUtils.isEmpty(word.trim()));
     }
+
     public static boolean isValidNumber(String number) {
 
         String regExpn = "^[0-9]{1,24}$";
@@ -50,6 +55,5 @@ public class Validation {
         return matcher.matches();
 
     }
-
 }
 
